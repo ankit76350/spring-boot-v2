fc37b970454de4a5d21767c7740ec451e91c7a32
ANKIT KUMAR <ankit@dsvcorp.com.au>
2026-03-31 16:16:13 +0530

    Intial commit

---
+++ src/main/java/com/example/demo/repositories/JournalEntryRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface JournalEntryRepository extends MongoRepository{
+
+    
+}
