4d1b831f6439dc351293d87e8da966e4f39e4b6b
ANKIT KUMAR <ankit@dsvcorp.com.au>
2026-04-01 13:07:43 +0530

    I have learn some basic thing here

--- src/main/java/com/example/demo/controllers/JournalEntryControllers.java
+++ src/main/java/com/example/demo/controllers/JournalEntryControllers.java
@@ -1,9 +1,15 @@
 package com.example.demo.controllers;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,15 +24,36 @@ public class JournalEntryControllers {
 
     @Autowired
     private JournalEntryService journalEntryService;
-    
-    @GetMapping
-    public List<JournalEntry>getAll(){
-        return null;
+
+    @GetMapping("/getAll")
+    public List<JournalEntry> getAll() {
+        return journalEntryService.getAllDocument();
     }
 
     @PostMapping("/save")
-    public boolean saveEntry(@RequestBody JournalEntry myEntry){
-        journalEntryService.saveNewEntry(myEntry);
-        return true;
+    public ResponseEntity<JournalEntry> saveEntry(@RequestBody JournalEntry myEntry) {
+        try {
+            myEntry.setDate(new Date());
+            journalEntryService.saveNewEntry(myEntry);
+            return new ResponseEntity<>(myEntry, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("/id/{myId}")
+    public ResponseEntity<JournalEntry> findDocumentById(@PathVariable String myId) {
+        Optional<JournalEntry> entryFind = journalEntryService.findDocumentById(myId);
+        if (entryFind.isPresent()) {
+            return new ResponseEntity<>(entryFind.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("/id/{myId}")
+    public ResponseEntity<?> deleteById(@PathVariable String myId) {
+        journalEntryService.deleteDocumentById(myId);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
--- src/main/java/com/example/demo/services/JournalEntryService.java
+++ src/main/java/com/example/demo/services/JournalEntryService.java
@@ -1,5 +1,9 @@
 package com.example.demo.services;
 import com.example.demo.entity.JournalEntry;
+
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -15,5 +19,21 @@ public class JournalEntryService {
         journalEntryRepository.save(entry);
     }
 
+
+    public List<JournalEntry> getAllDocument() {
+        return journalEntryRepository.findAll();
+    }
+
+
+    public Optional<JournalEntry> findDocumentById(String myId){
+        return journalEntryRepository.findById(myId);
+    }
+    
+
+    public void deleteDocumentById(String myId){
+        journalEntryRepository.deleteById(myId);
+    }
+
+    
 
 }
