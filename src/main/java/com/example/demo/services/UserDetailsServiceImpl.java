6d90d64a40c3ac33f76310a4393c99d6545f3501
ANKIT KUMAR <ankit@dsvcorp.com.au>
2026-04-02 11:44:30 +0530

    I have written the code user authentication thing here

---
+++ src/main/java/com/example/demo/services/UserDetailsServiceImpl.java
@@ -0,0 +1,33 @@
+package com.example.demo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import com.example.demo.repositories.UserRepository;
+
+@Component
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        com.example.demo.entity.User user = userRepository.findByUserName(userName);
+
+        if (user != null) {
+             return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getUserName())
+                .password(user.getPassword())
+                .roles(user.getRoles().toArray(new String[0]))
+                .build();
+
+                
+        }
+
+        throw new UsernameNotFoundException("User not found with username: " + userName);
+    }
+}
