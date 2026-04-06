6aa75996f150f2d5dc42a5f7e06734b3ed2fa4e1
ANKIT KUMAR <ankit@dsvcorp.com.au>
2026-04-06 13:37:04 +0530

    this is the one way to loging in the file and cosole both place

--- src/main/java/com/example/demo/services/UserService.java
+++ src/main/java/com/example/demo/services/UserService.java
@@ -7,8 +7,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.bson.types.ObjectId;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
