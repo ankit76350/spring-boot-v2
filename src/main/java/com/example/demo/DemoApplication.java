7dea03c19f4ed38a32b3793e483dae95ed6e3812
ANKIT KUMAR <ankit@dsvcorp.com.au>
2026-04-06 12:30:46 +0530

    Loging thing is streted

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,7 +2,9 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.MongoTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -13,7 +15,9 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(DemoApplication.class, args);
+		ConfigurableEnvironment environment = context.getEnvironment();
+		System.out.println(environment.getActiveProfiles()[0]);
 	}
 
 	@Bean
--- src/main/java/com/example/demo/config/SpringSecurity.java
+++ src/main/java/com/example/demo/config/SpringSecurity.java
@@ -19,7 +19,6 @@ import com.example.demo.services.UserDetailsServiceImpl;
 
 @Configuration
 @EnableWebSecurity
-@Profile("dev")
 public class SpringSecurity {
 
     @Autowired
