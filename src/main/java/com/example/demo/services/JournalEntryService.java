a9f4e77b5bfb815a778008ae1bd61b1ea4c8c6ea
ANKIT KUMAR <ankit@dsvcorp.com.au>
2026-04-08 10:41:31 +0530

    I have learn about the @service @component stuff

--- src/main/java/com/example/demo/services/JournalEntryService.java
+++ src/main/java/com/example/demo/services/JournalEntryService.java
@@ -11,11 +11,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.demo.repositories.JournalEntryRepository;
 
-@Component
+@Service
 public class JournalEntryService {
     @Autowired
     private JournalEntryRepository journalEntryRepository;
